d29f9cc3e3d61f0e17c1fc3082e843b2016f3410
Lex <akimov.infotech@gmail.com>
2016-12-20 18:42:27 +0600

    GeoQuiz

---
+++ GeoQuiz/app/src/main/java/com/akimov/android/geoquiz/Question.java
@@ -0,0 +1,31 @@
+package com.akimov.android.geoquiz;
+
+/**
+ * Created by Alexander on 4/20/2016.
+ */
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+}
