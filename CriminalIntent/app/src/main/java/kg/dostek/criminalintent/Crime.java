5541d5d44e946f2a3c8dfb91cd0f2f8ec6719711
Lex <akimov.infotech@gmail.com>
2016-12-20 18:41:59 +0600

    CrimainalIntent

---
+++ CriminalIntent/app/src/main/java/kg/dostek/criminalintent/Crime.java
@@ -0,0 +1,50 @@
+package kg.dostek.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by Alexander on 5/13/2016.
+ */
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    public UUID getId() {
+        return mId;
+
+    }
+
+    public Crime() {
+    //Generate uniq
+        mId = UUID.randomUUID();
+        mDate = new Date();
+
+    }
+}
