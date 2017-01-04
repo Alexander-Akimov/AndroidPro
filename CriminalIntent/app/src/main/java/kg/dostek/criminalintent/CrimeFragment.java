d10180ef4353628c83f67601a0b81277e43aa255
Lex <akimov.infotech@gmail.com>
2017-01-04 18:15:08 +0600

    send data form crimeListFragment to Crime fragement using activity intent

--- CriminalIntent/app/src/main/java/kg/dostek/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/kg/dostek/criminalintent/CrimeFragment.java
@@ -14,6 +14,7 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 
+import java.util.UUID;
 import java.util.zip.Inflater;
 
 /**
@@ -28,7 +29,12 @@ public class CrimeFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mCrime = new Crime();
+       // mCrime = new Crime();
+
+        UUID crimeId = (UUID) getActivity().getIntent().getSerializableExtra(CrimeActivity.EXTRA_CRIME_ID);
+
+        mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
+
     }
 
     @Nullable
@@ -37,6 +43,7 @@ public class CrimeFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
 
         mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mTitleField.setText(mCrime.getTitle());
         mTitleField.addTextChangedListener(new TextWatcher() {
 
             @Override
@@ -60,6 +67,7 @@ public class CrimeFragment extends Fragment {
         mDateButton.setEnabled(false);
 
         mSolvedCheckButton = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckButton.setChecked(mCrime.isSolved());
         mSolvedCheckButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
