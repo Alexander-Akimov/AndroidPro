f62bdd810936570a09715476fe4443a55e88a8e4
akimov.infotech <akimov.infotech@gmail.com>
2018-11-25 15:43:09 +0700

    gitignore and cheatactivity

---
+++ GeoQuiz/app/src/main/java/com/akimov/geoquiz/presentation/main/view/CheatActivity.kt
@@ -0,0 +1,12 @@
+package com.akimov.geoquiz.presentation.main.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CheatActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_cheat)
+  }
+}
