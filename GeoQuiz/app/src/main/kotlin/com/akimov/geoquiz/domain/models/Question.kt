85d5a76cf80fbcd479eded851feada097ef1c065
akimov.infotech <akimov.infotech@gmail.com>
2019-01-10 17:26:51 +0600

    change modifier to data class

--- GeoQuiz/app/src/main/kotlin/com/akimov/geoquiz/domain/models/Question.kt
+++ GeoQuiz/app/src/main/kotlin/com/akimov/geoquiz/domain/models/Question.kt
@@ -3,7 +3,7 @@ package com.akimov.geoquiz.domain.models
 /**
  * Created by lex on 1/5/18.
  */
-open class Question(var id: Int,
+data class Question(var id: Int,
                     var questionText: String,
                     var isAnswerTrue: Boolean,
                     var description: String,
